4deb7343f5705760674389b1384f7f29e5047017
Xeyth <30859995+Xeythhhh@users.noreply.github.com>
2025-01-26 01:44:53 +0200

    Added some information to Xeyth.FunctionalExtensions and code to make sure it produces and artifact during packing

---
+++ Xeyth.FunctionalExtensions/FunctionalExtensionsInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace Xeyth.FunctionalExtensions;
+
+/// <summary>This class serves no functional purpose and exists only to ensure the assembly produces a valid artifact.</summary>
+public static class FunctionalExtensionsInfo
+{
+    /// <summary>A meaningless constant.</summary>
+    public const string Purpose = "This package only references other packages.";
+}
--- Xeyth.FunctionalExtensions/Info.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Xeyth.FunctionalExtensions;
-
-/// <summary>
-/// This assembly simply references all the functional extensions packages to avoid having to add all of them individually
-/// </summary>
-internal class Info;
