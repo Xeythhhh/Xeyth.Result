99a3c6a1aadc6f410954ffc44f75a706af6439cb
Xeyth <30859995+Xeythhhh@users.noreply.github.com>
2025-01-26 01:56:13 +0200

    Added a module initializer to Xeyth.FunctionalExtensions package to ensure it produces an artifact

--- Xeyth.FunctionalExtensions/FunctionalExtensionsInfo.cs
+++ Xeyth.FunctionalExtensions/FunctionalExtensionsInfo.cs
@@ -1,8 +1,16 @@
-﻿namespace Xeyth.FunctionalExtensions;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+
+namespace Xeyth.FunctionalExtensions;
 
 /// <summary>This class serves no functional purpose and exists only to ensure the assembly produces a valid artifact.</summary>
 public static class FunctionalExtensionsInfo
 {
     /// <summary>A meaningless constant.</summary>
     public const string Purpose = "This package only references other packages.";
+
+    [ModuleInitializer]
+    [SuppressMessage("Usage", "CA2255:The 'ModuleInitializer' attribute should not be used in libraries", Justification = "<Pending>")]
+    [SuppressMessage("CodeQuality", "IDE0079:Remove unnecessary suppression", Justification = "<Pending>")]
+    public static void Initialize() { }
 }
