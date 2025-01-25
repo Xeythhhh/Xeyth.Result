7ef8cdc94cb4c6625b4305fc1a5581bfdd7457a3
Xeyth <30859995+Xeythhhh@users.noreply.github.com>
2025-01-26 01:37:51 +0200

    Added Info class on Xeyth.FunctionalExtensions package

---
+++ Xeyth.FunctionalExtensions/Info.cs
@@ -0,0 +1,6 @@
+﻿namespace Xeyth.FunctionalExtensions;
+
+/// <summary>
+/// This assembly simply references all the functional extensions packages to avoid having to add all of them individually
+/// </summary>
+internal class Info;
