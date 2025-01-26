4305a10c940d28a8292998ee5387173c7b4ac245
Xeyth <30859995+Xeythhhh@users.noreply.github.com>
2025-01-26 02:05:18 +0200

    Added abritrary value to force git-diff for Xeyth.FunctionalExtensions package

--- Xeyth.FunctionalExtensions/FunctionalExtensionsInfo.cs
+++ Xeyth.FunctionalExtensions/FunctionalExtensionsInfo.cs
@@ -9,6 +9,10 @@ public static class FunctionalExtensionsInfo
     /// <summary>A meaningless constant.</summary>
     public const string Purpose = "This package only references other packages.";
 
+    [SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "<Pending>")]
+    [SuppressMessage("Roslynator", "RCS1213:Remove unused member declaration", Justification = "<Pending>")]
+    private const int ArbitraryNumberToForceChangesOnGit = 69;
+
     [ModuleInitializer]
     [SuppressMessage("Usage", "CA2255:The 'ModuleInitializer' attribute should not be used in libraries", Justification = "<Pending>")]
     [SuppressMessage("CodeQuality", "IDE0079:Remove unnecessary suppression", Justification = "<Pending>")]
